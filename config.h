3f3d6867b4a9fded47ef75a385927de0503d0f14
Rahit <rahitkar@users.noreply.github.com>
2020-06-07 01:41:36 +0530

    added config file from which the prompt name can be configerd

---
+++ config.h
@@ -0,0 +1,1 @@
+char prompt[] = "Rahit";
--- shell.c
+++ shell.c
@@ -5,12 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "config.h"
+
 typedef char *Char_ptr;
 
 Char_ptr *split(char *text, char *symbol)
 {
   int counter = 0;
-  Char_ptr* text_array_ptr = calloc(counter + 1, sizeof(char *));
+  Char_ptr *text_array_ptr = calloc(counter + 1, sizeof(char *));
   text_array_ptr[counter] = &text[0];
 
   for (int i = 0; text[i] != '\0'; i++)
@@ -32,33 +34,28 @@ Char_ptr *split(char *text, char *symbol)
 
 int main(void)
 {
+  Char_ptr shell_ditels = strcmp(prompt, "") == 0 ? "my_shell" : prompt;
+  
   char command[300];
-
-  printf("my_shell_$-> ");
+  printf("%s_$-> ", shell_ditels);
   while (1)
   {
     gets(command);
-
     if (!strcmp(command, "exit"))
     {
       return 0;
     }
-    
-    // Char_ptr args[] = {command, NULL};
-    Char_ptr* args = split(command, " ");
+    Char_ptr *args = split(command, " ");
     int pid = fork();
     if (pid == 0)
     {
-      // printf("child%d\n", pid);
       execvp(args[0], args);
       printf("commad not found\n");
-      return 1;
     }
     else
     {
       wait(&pid);
-      // printf("parent%d\n", pid);
     }
-    printf("my_shell_$-> ");
+  printf("%s_$-> ", shell_ditels);
   }
 }
