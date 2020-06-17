6b2ca09398cc7e833e119bdccc8eb076df3c3433
Rahit <rahitkar@users.noreply.github.com>
2020-06-18 05:23:17 +0530

    modified shell.c for cd and .. and also used signal calls

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #include "config.h"
 
@@ -35,27 +36,44 @@ Char_ptr *split(char *text, char *symbol)
 int main(void)
 {
   Char_ptr shell_ditels = strcmp(prompt, "") == 0 ? "my_shell" : prompt;
-  
+
   char command[300];
-  printf("%s_$-> ", shell_ditels);
+  char current_directory[100];
   while (1)
   {
+    signal(SIGINT, SIG_IGN);
+    printf("%s_$%s-> ", shell_ditels, getcwd(current_directory, 100));
     gets(command);
     if (!strcmp(command, "exit"))
-    {
       return 0;
-    }
+
     Char_ptr *args = split(command, " ");
-    int pid = fork();
+    if (!strcmp(args[0], "..")) //builtIn commands
+    {
+      chdir(args[0]);
+      continue;
+    }
+
+    if (!strcmp(args[0], "cd"))
+    {
+      getcwd(current_directory, 100);
+      char *dir = strcat(current_directory, "/");
+      chdir(strcat(dir, args[1]));
+      continue;
+    }
+
+    int pid = fork(); // exac part
     if (pid == 0)
     {
+      signal(SIGINT, NULL);
       execvp(args[0], args);
       printf("commad not found\n");
+      exit(-1);
     }
     else
     {
       wait(&pid);
     }
-  printf("%s_$-> ", shell_ditels);
+    printf("\n");
   }
 }
